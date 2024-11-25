bd0c1fb19fead2fcaae810462475bc2c29b3f13e
李龙 <lilong2576@hanweb.com>
2024-11-25 11:35:44 +0800

    feat: useRef 获取dom

---
+++ src/App6.tsx
@@ -0,0 +1,18 @@
+import { useEffect, useRef } from "react";
+
+function App() {
+    // 获取dom
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+        inputRef.current?.focus();
+    });
+
+    return (
+        <div>
+            <input ref={inputRef}></input>
+        </div>
+    );
+}
+
+export default App;
